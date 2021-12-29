1807064d00e79456fdaf1ea5f9888d340609e4fb
Ranjithkumar PC <ranjithkumarpc94@gmail.com>
2021-12-29 10:50:22 +0530

    Initial Commit

---
+++ src/main/java/com/example/demo/controller/StringUtilController.java
@@ -0,0 +1,12 @@
+package com.example.demo.controller;
+
+public class StringUtilController {
+
+	public static String reverseString(String s) {
+		String output = "";
+		for(int i = s.length()-1; i >=0 ; i--) {
+			output += s.charAt(i);
+		}
+		return output;
+	}
+}
---
+++ src/test/java/com/example/demo/controller/StringUtilControllerTest.java
@@ -0,0 +1,56 @@
+package com.example.demo.controller;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class StringUtilControllerTest {
+
+	@Test
+	public void testReverseString() {
+		String reversed = StringUtilController.reverseString("test");
+		assertEquals("tset", reversed);
+	}
+
+	@Test
+	public void test() {
+		List<String> spyList = Mockito.spy(ArrayList.class);
+		List<String> mockList = Mockito.mock(ArrayList.class);
+
+		assertEquals(0, spyList.size());
+		Mockito.doReturn(100).when(spyList).size();
+		assertEquals(100, spyList.size());
+
+		assertEquals(0, mockList.size());
+		Mockito.doReturn(10).when(mockList).size();
+		assertEquals(10, mockList.size());
+
+		System.out.println(spyList.size());
+		System.out.println(mockList.size());
+	}
+
+	@Test
+	public void whenCreateMock_thenCreated() {
+		List mockedList = Mockito.mock(ArrayList.class);
+		mockedList.add("one");
+		Mockito.verify(mockedList).add("one");
+		assertEquals(0, mockedList.size());
+		System.out.println(mockedList.size());
+	}
+
+	@Test
+	public void whenCreateSpy_thenCreate() {
+		List spyList = Mockito.spy(ArrayList.class);
+		spyList.add("one");
+		Mockito.verify(spyList).add("one");
+		assertEquals(1, spyList.size());
+		System.out.println(spyList.size());
+	}
+}
