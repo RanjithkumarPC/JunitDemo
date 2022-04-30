33934e188be6e48c2115b674c85ebffcd03f4850
RanjithkumarPC <ranjithkumarpc94@gmail.com>
2022-04-30 16:26:59 +0530

    added sysout commit

--- src/main/java/com/example/demo/JunitDemoApplication.java
+++ src/main/java/com/example/demo/JunitDemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JunitDemoApplication {
 
 	public static void main(String[] args) {
+		System.out.println("wqasad");
 		SpringApplication.run(JunitDemoApplication.class, args);
 	}
 
